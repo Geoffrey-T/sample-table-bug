28f724d621d67682f3c82552b35e8b8aeaf1e818
Geoffrey <geoffrey.thenot@gmail.com>
2016-11-23 11:40:04 +0100

    Initial Commit

---
+++ sample-table-bug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sample-table-bug
+//
+//  Created by geoffrey thenot on 23/11/2016.
+//  Copyright © 2016 tiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sample-table-bug/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sample-table-bug
+//
+//  Created by geoffrey thenot on 23/11/2016.
+//  Copyright © 2016 tiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
